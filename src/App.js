63ef175a5a063c98863475f9cdcc3c0dac26dd9c
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2018-02-22 13:18:49 -0700

    created repo and uploaded work

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./App.css";
+import data from "./data";
+import Typist from 'react-typist';
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      originalList: data,
+      modifiedList: data,
+      message: "",
+    };
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+
+
+  handleDeletion(element) {
+    let newItem = element;
+    let message = " Oh no, you deleted all the sports :( ";
+    const sports = this.state.modifiedList.filter(
+      sport => sport.id !== newItem.id
+    );
+    if (sports.length === 0) {
+      this.setState({ message: message });
+    }
+    this.setState({ modifiedList: sports });
+  }
+
+  handleReset() {
+    this.setState({ modifiedList: this.state.originalList });
+    this.setState({ message: "" });
+  }
+
+  render() {
+    let sport = this.state.modifiedList.map((element, i) => {
+      return (
+        <section key={i}>
+          <div
+            className="individual_sport"
+            onClick={e => this.handleDeletion(element)}
+            value={element.id}
+            defaultChecked={false}
+          >
+            {element.name}
+          </div>
+        </section>
+      );
+    });
+    return (
+      <main className="main_bod">
+        <nav className="nav_bar" />
+        <div className="wrapper">
+        <section className="small_section">
+          <Typist>
+          <h2 className='title'>
+            Click on your least favorite sports below to delete them until you
+            have three left
+          </h2>
+          </Typist>
+        </section>
+        <section className="sports_container">
+          <div className="message">{this.state.message}</div>
+          {sport}
+        </section>
+        <section className="small_section">
+          <button onClick={this.handleReset} className='reset_btn'>Reset</button>
+        </section>
+        </div>
+        <footer className="footer" />
+      </main>
+    );
+  }
+}
---
+++ src/data.js
@@ -0,0 +1,10 @@
+module.exports=[
+    { id: 'ab123aa', name: 'Baseball' },
+    { id: 'ab124aa', name: 'Golf' },
+    { id: 'ab125aa', name: 'Hockey' },
+    { id: 'ab126bb', name: 'Soccer' },
+    { id: 'ab127bb', name: 'Football' },
+    { id: 'ab128cc', name: 'Skiing' },
+    { id: 'ab129dd', name: 'Ping Pong' },
+    { id: 'ab130cc', name: 'Foosball' }
+]
