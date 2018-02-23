e2270c9a6ed0f054bb7ef684b25564a2bbcf1aaf
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2018-02-22 21:07:25 -0700

    finalized

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,36 @@
 // THESE IMPORTATIONS ARE TO TEST THE APP COMPONENT //
-import React from 'react';
-import { configure } from 'enzyme'; 
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, mount, render } from 'enzyme'; 
-configure({ adapter: new Adapter() });
+// import React from 'react';
+// import { configure } from 'enzyme'; 
+// import Adapter from 'enzyme-adapter-react-16';
+// import { shallow, mount, render } from 'enzyme'; 
+// configure({ adapter: new Adapter() });
 
-import renderer from 'react-test-renderer';
-import App from './App.js';
+// import renderer from 'react-test-renderer';
+// import App from './App.js';
 
 
 // THIS CONSTANT IS TO TEST THE DATA //
-// const functions = require("./functions.js");
+const functions = require("./functions.js");
 
 
 
-describe (`Testing the App Component`,()=>{
-  test()
-})
+// describe (`Testing the App Component`,()=>{
+//   test()
+// })
 
 
 
 
-// DATA TESTING // S
-// describe(`Sports information`, () => {
-//   test(`expect data format to be an array`, () => {
-//     let result = functions.sportsFormat();
-//     expect(Array.isArray(result));
-//   });
-//   test(`Length of data array should be 8`, () => {
-//     let result = functions.confirmLength();
-//     expect(result).toEqual(8);
-//   });
-// });
+// DATA TESTING //
+describe(`Sports information`, () => {
+  test(`expect data format to be an array`, () => {
+    let result = functions.sportsFormat();
+    expect(Array.isArray(result));
+  });
+  test(`Length of data array should be 8`, () => {
+    let result = functions.confirmLength();
+    expect(result).toEqual(8);
+  });
+});
 
 
