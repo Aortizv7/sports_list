1a2bdcc437deb18afb3815047971b3c6857125e2
Aortizv7 <ana.ortiz.y.b.7@gmail.com>
2018-07-31 11:33:41 -0600

    basic code clean up

--- src/App.js
+++ src/App.js
@@ -1,70 +1,75 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 import data from "./data";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      originalList: data,
-      modifiedList: data,
-      message: ""
-    };
-    this.handleReset = this.handleReset.bind(this);
-  }
-
-  handleDeletion(element) {
-    let newItem = element;
-    let message = " Oh no, you deleted all the sports :( ";
-    const sports = this.state.modifiedList.filter(
-      sport => sport.id !== newItem.id
-    );
-    if (sports.length === 0) {
-      this.setState({ message: message });
+    constructor() {
+        super();
+        this.state = {
+            originalList: data,
+            modifiedList: data,
+            message: ""
+        };
+        this.handleReset = this
+            .handleReset
+            .bind(this);
     }
-    this.setState({ modifiedList: sports });
-  }
 
-  handleReset() {
-    this.setState({ modifiedList: this.state.originalList });
-    this.setState({ message: "" });
-  }
+    handleDeletion(element) {
+        let newItem = element;
+        let message = " Oh no, you deleted all the sports :( ";
+        const sports = this
+            .state
+            .modifiedList
+            .filter(sport => sport.id !== newItem.id);
+        if (sports.length === 0) {
+            this.setState({message: message});
+        }
+        this.setState({modifiedList: sports});
+    }
 
-  render() {
-    let sport = this.state.modifiedList.map((element, i) => {
-      return (
-        <section key={i}>
-          <div
-            className="individual_sport"
-            onClick={e => this.handleDeletion(element)}
-            value={element.id}
-            defaultChecked={false}
-          >
-            {element.name}
-          </div>
-        </section>
-      );
-    });
-    return (
-      <main className="main_bod">
-        <div className="wrapper">
-          <section className="small_section">
-              <h2 className="title">
-                Click on your least favorite sports below, to delete them until
-                you have three left...
-              </h2>
-          </section>
-          <section className="sports_container">
-            <div className="message">{this.state.message}</div>
-            {sport}
-          </section>
-          <section className="small_section">
-            <button onClick={this.handleReset} className="reset_btn">
-              Reset
-            </button>
-          </section>
-        </div>
-      </main>
-    );
-  }
+    handleReset() {
+        this.setState({modifiedList: this.state.originalList});
+        this.setState({message: ""});
+    }
+
+    render() {
+        let sport = this
+            .state
+            .modifiedList
+            .map((element, i) => {
+                return (
+                    <section key={i}>
+                        <div
+                            className="individual_sport"
+                            onClick={e => this.handleDeletion(element)}
+                            value={element.id}
+                            defaultChecked={false}>
+                            {element.name}
+                        </div>
+                    </section>
+                );
+            });
+        return (
+            <main className="main_bod">
+                <div className="wrapper">
+                    <section className="small_section">
+                        <h2 className="title">
+                            Click on your least favorite sports below, to delete them until you have three
+                            left...
+                        </h2>
+                    </section>
+                    <section className="sports_container">
+                        <div className="message">{this.state.message}</div>
+                        {sport}
+                    </section>
+                    <section className="small_section">
+                        <button onClick={this.handleReset} className="reset_btn">
+                            Reset
+                        </button>
+                    </section>
+                </div>
+            </main>
+        );
+    }
 }
